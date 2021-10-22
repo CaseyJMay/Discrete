d0b695b78e087443fbe05db287ba25a458053dee
clee4 <42856774+clee4@users.noreply.github.com>
2021-10-22 11:00:47 -0400

    added win evaluation function

--- minimax.py
+++ minimax.py
@@ -1,8 +1,25 @@
 ### Implementation of the Minimax algorithm on tic tac toe
 
+computer = 1
+human = -1
+
 game_board = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
 
-
+def game_finished(state, player):
+    win_state = [
+        [state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[2][0], state[1][1], state[0][2]],
+    ]
+    if [player, player, player] in win_state:
+        return True
+    else:
+        return False
 
 #function to find the best possible move given the current game board
 def best_move(board):
